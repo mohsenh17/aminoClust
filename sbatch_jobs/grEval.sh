475e40082b2d553974f675190d3aadf1014a3360
mohsenh17 <mohsen.hosseini1272@gmail.com>
2025-04-22 13:03:35 -0400

    slurm job reorganize + eval slurm

---
+++ sbatch_jobs/grEval.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --time=0:40:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --output=slurm_output/eval-%j.out  #%j for jobID
+
+source /home/mohsenh/projects/def-ilie/mohsenh/ENV/prostt5ENV/bin/activate
+python evaluation/cluster_eval.py
