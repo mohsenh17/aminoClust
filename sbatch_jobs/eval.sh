ab5548ff7f8e5a5668cdc31f4de3f0e0eb2e662e
mohsenh17 <mohsen.hosseini1272@gmail.com>
2025-04-23 10:48:46 -0400

    reduce mem usage in eval

--- sbatch_jobs/eval.sh
+++ sbatch_jobs/eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=0:40:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=4G
 #SBATCH --output=slurm_output/eval-%j.out  #%j for jobID
 
 source /home/mohsenh/projects/def-ilie/mohsenh/ENV/prostt5ENV/bin/activate
